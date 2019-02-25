04feb276bea4fa19fcf4ccb78fb9d1c60b75ea2b
Jason Snider <root@jasonsnider.com>
2019-02-24 22:14:54 -0600

    Accept the date from the URL

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 import { ApodService } from '../api/apod.service';
 import { Apod } from '../models/apod';
 
@@ -12,18 +12,23 @@ export class ApodComponent implements OnInit {
 
   apod:Apod;
 
-  constructor(private apodService: ApodService) { }
+  constructor(
+    private apodService: ApodService,
+    private router: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.getApod();
+    this.router.params.subscribe((params) => {
+      this.getApod(params['date']);
+    });
   }
 
-  getApod(): void{
-    let date = new Date().toISOString().slice(0,10);
+  getApod(date:string): void{
 
     this.apodService.getApod(date).subscribe(
       (response:any)=>{
         this.apod = response;
+        console.log(response);
       }
     );
 
